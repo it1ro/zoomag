12721d13d34dc3539b5ff005616a70beaa5ece19
it1ro <code.for.func@gmail.com>
2025-11-25 00:42:14 +0400

    save

--- Views/AdminWindow.xaml.cs
+++ Views/AdminWindow.xaml.cs
@@ -1,60 +1,13 @@
-﻿// File: Zoomag/Views/AdminWindow.xaml.cs
+﻿using System.Windows;
+using Zoomag.Views;
 
 namespace Zoomag.Views;
 
-using System.Windows;
-using System.Windows.Controls; // Добавлено для ComboBox
-using Data;
-using Models;
-using Reports;
-
-/// <summary>
-/// Interaction logic for AdminWindow.xaml
-/// </summary>
-public partial class AdminWindow : Window, IDisposable
+public partial class AdminWindow : Window
 {
-    // Удаляем поле _context из класса.
-
     public AdminWindow()
     {
         InitializeComponent();
-    }
-
-    public void Dispose()
-    {
-        // Не нужно больше уничтожать _context здесь, так как он больше не поле класса.
-        // Однако, если у окна есть другие ресурсы, их можно освободить.
-        GC.SuppressFinalize(this);
-    }
-
-
-
-    private void ShowArrival(object sender, RoutedEventArgs e)
-    {
-        var arrivalWindow = new ArrivalWindow();
-        Hide();
-        arrivalWindow.Show();
-    }
-
-    private void ShowSale(object sender, RoutedEventArgs e)
-    {
-        var saleWindow = new SaleWindow();
-        Hide();
-        saleWindow.Show();
-    }
-
-    private void ShowStock(object sender, RoutedEventArgs e)
-    {
-        var stockWindow = new StockWindow();
-        Hide();
-        stockWindow.Show();
-    }
-
-    private void ShowReports(object sender, RoutedEventArgs e)
-    {
-        var reportsWindow = new AdminReportsWindow();
-        Hide();
-        reportsWindow.Show();
     }
 
     private void OpenProductEditor(object sender, RoutedEventArgs e)
@@ -76,5 +29,40 @@ public partial class AdminWindow : Window, IDisposable
         var editor = new UnitsEditorWindow();
         Hide();
         editor.Show();
+    }
+
+    private void ShowArrival(object sender, RoutedEventArgs e)
+    {
+        var arrival = new ArrivalWindow();
+        Hide();
+        arrival.Show();
+    }
+
+    private void ShowSale(object sender, RoutedEventArgs e)
+    {
+        var sale = new SaleWindow();
+        Hide();
+        sale.Show();
+    }
+
+    private void ShowStock(object sender, RoutedEventArgs e)
+    {
+        var stock = new StockWindow();
+        Hide();
+        stock.Show();
+    }
+
+    private void ShowReports(object sender, RoutedEventArgs e)
+    {
+        var reports = new Reports.AdminReportsWindow();
+        Hide();
+        reports.Show();
+    }
+
+    private void Logout(object sender, RoutedEventArgs e)
+    {
+        var login = new LoginWindow();
+        Hide();
+        login.Show();
     }
 }
