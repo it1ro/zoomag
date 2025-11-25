31c22bc536fcee096c0e114238f40ea1799a7bbc
it1ro <code.for.func@gmail.com>
2025-11-25 07:31:43 +0400

    save

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -19,7 +19,7 @@ public class AppDbContext : DbContext
         if (!optionsBuilder.IsConfigured)
             // В реальных проектах строку подключения получают из IConfiguration (appsettings.json)
             optionsBuilder.UseSqlServer(
-                "Server=(localdb)\\mssqllocaldb;Database=ValeevaZoomag6;Trusted_Connection=true;TrustServerCertificate=true;",
+                "Server=(localdb)\\mssqllocaldb;Database=valeeva_zoomag;Trusted_Connection=true;TrustServerCertificate=true;",
                 options => options.EnableRetryOnFailure(3));
     }
 
